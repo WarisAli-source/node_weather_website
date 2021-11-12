3d251b97ba0efe28419258aef393740dfb2f8840
WarisAli-source <warissid41@gmail.com>
2021-11-12 18:59:25 +0530

    second commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,7 @@
-console.log('Client side javascript file is loaded')
-
-
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-
-
 
 
 weatherform.addEventListener('submit' , (e) => {
